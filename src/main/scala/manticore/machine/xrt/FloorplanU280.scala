717d7cffa8b40dea0fd59f963069b3016c7eed8b
Sahand Kashani <sahand.kashani@gmail.com>
2022-09-19 14:01:05 +0200

    Add U280 floorplan and root clock at center of device

--- src/main/scala/manticore/machine/xrt/FloorplanU280.scala
+++ src/main/scala/manticore/machine/xrt/FloorplanU280.scala
@@ -3,7 +3,7 @@ package manticore.machine.xrt
 import collection.mutable.{Map => MMap}
 
 trait U280Floorplan extends Floorplan {
-  def getManticoreKernelInstName(): String = "level0_i/level1/level1_i/ulp/ManticoreKernel_1/inst"
+  def getManticoreKernelInstName(): String = "level0_i/ulp/ManticoreKernel_1/inst"
 }
 
 object U280FloorplanImpl {
@@ -12,33 +12,31 @@ object U280FloorplanImpl {
   case class ArbitraryPblock(name: String, resources: String) extends Pblock
 
   // format: off
-  val nonShellPblock = ArbitraryPblock("pblock_slr2_slr3", "{ SLR2:SLR3 }")
+  val slr2Slr3Pblock = ArbitraryPblock("pblock_slr0_slr1_slr2", "{ SLR0:SLR2 }")
   // format: on
 
   // Cores are placed as follows:
-  // - Place all cores in a single large pblock that spans SLR2 and SLR3
+  // - Place all cores in a single large pblock that spans SLR0:SLR2.
   //
   // Switches are placed as follows:
-  // - Place all switches in a single large pblock that spans SLR2 and SLR3.
-  object LooseNonShell extends U280Floorplan {
+  // - Place all switches in a single large pblock that spans SLR0:SLR2.
+  object LooseNonShell extends U250Floorplan {
     // The anchor doesn't matter as we are leaving the cores in a single large pblock
-    // that spans 2 SLRs.
+    // that spans all SLRs.
     val anchor = GridLoc(0, 0)
 
-    def getRootClock(): ClockRegion = ClockRegion(4, 11)
+    def getRootClock(): ClockRegion = ClockRegion(4, 6)
 
     def getCoreToPblockMap(dimX: Int, dimY: Int): Map[TorusLoc, ArbitraryPblock] = {
       val gridToTorus = getGridLocToTorusLocMap(dimX, dimY, anchor)
 
       gridToTorus.map { case (gridLoc, torusLoc) =>
-        torusLoc -> nonShellPblock
+        torusLoc -> slr2Slr3Pblock
       }
     }
 
     def getSwitchToPblockMap(dimX: Int, dimY: Int): Map[TorusLoc, ArbitraryPblock] = {
       getCoreToPblockMap(dimX, dimY)
     }
-
-    override def toTcl(dimX: Int, dimY: Int): String = ""
   }
 }
