a076c712de1c36d701d8d73b8c465c199fb8a80f
Sahand Kashani <sahand.kashani@gmail.com>
2022-10-05 09:35:32 +0200

    Fix minimum reset pulse width

--- src/main/scala/manticore/machine/core/Management.scala
+++ src/main/scala/manticore/machine/core/Management.scala
@@ -85,9 +85,9 @@ class Management(dimX: Int, dimY: Int) extends Module {
   val state = RegInit(sIdle)
 
   // Very conservative countdown value. Can technically be matched to the exact latency of the SoftResetTree.
-  // I cap the minimum to 10 to ensure that even small arrays like 2x2 have enough time for the reset to propagate
-  // to the SLRs before they fan out to the cores. It is a conservative number.
-  val soft_reset_value           = math.min(10, 2 * dimX * dimY)
+  // I cap the minimum value to 32 cycles to ensure that even small arrays like 2x2 have enough time for the reset
+  // to propagate to the SLRs before they fan out to the cores. It is a conservative number and can be reduced.
+  val soft_reset_value           = math.max(32, 2 * dimX * dimY)
   val soft_reset_countdown_timer = Reg(UInt(unsignedBitLength(soft_reset_value).W))
 
   val done_out = RegNext(state === sDone)
